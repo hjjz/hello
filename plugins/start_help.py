3eeb6a871b76c4551063e5b583016da39a46175b
hjjz <107469466+hjjz@users.noreply.github.com>
2022-07-03 06:21:25 -0700

    Update start_help.py

--- plugins/start_help.py
+++ plugins/start_help.py
@@ -10,7 +10,7 @@ from helper.importCommon import *
 @Client.on_message(filters.private & filters.command("start"))
 async def start_handler(bot, update):
     if await search_user_in_community(bot, update):
-        await update.reply_text(BotMessage.start_msg, parse_mode = 'html')
+        await update.reply_text(BotMessage.start_msg)
     return checking_user_in_db(update.chat.id)
 
 
@@ -18,6 +18,6 @@ async def start_handler(bot, update):
 @Client.on_message(filters.private & filters.command("help"))
 async def help_handler(bot, update):
     if await search_user_in_community(bot, update):
-        await update.reply_text(BotMessage.help_msg, parse_mode = 'html')
+        await update.reply_text(BotMessage.help_msg)
     return
 
